79f9f67410e86a322bc2be43e6be28a292b7c712
Emerson Ribeiro <emersonrjr03@gmail.com>
2021-10-29 20:34:35 +0100

    Adding actuator to the API

--- src/main/java/com/herms/hermscoder/config/SecurityConfig.java
+++ src/main/java/com/herms/hermscoder/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET,"/project/**")
                 .permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
